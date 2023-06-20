943c079032b7427f88e701d032b7abc8f78e1016
Wellyson E Brito <wellysone.brito@gmail.com>
2023-06-20 10:06:47 -0300

    init project

---
+++ app/configuration/connect.php
@@ -0,0 +1,28 @@
+<?php
+
+define("HOST", "localhost");
+define("DATABASENAME", "to-do-list-db");
+define("USER", "admin");
+define("PASSWORD", "w[[8EN9ThU4MD)0w");
+
+class Connect
+{
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->connectDatabase();
+    }
+
+    public function connectDatabase()
+    {
+        try {
+            $this-> connection = new PDO("mysql:host=". HOST. ";dbname=". DATABASENAME, USER, PASSWORD);
+        } catch(PDOException $e) {
+            echo("Error: ".$e->getMessage());
+            die();
+        }
+    }
+}
+
+$testConnection = new Connect();

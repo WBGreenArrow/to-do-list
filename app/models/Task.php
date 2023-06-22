a3f80379548d8f7e748beda622cf81a2b4518167
Wellyson E Brito <wellysone.brito@gmail.com>
2023-06-22 16:25:27 -0300

    feat: created method create task in model

--- app/models/Task.php
+++ app/models/Task.php
@@ -16,7 +16,51 @@ class TaskModel extends Connect
     {
         $sqlSelect = $this->connection->query("SELECT * FROM $this->table");
         $resultQuery = $sqlSelect->fetchAll(PDO::FETCH_ASSOC);
+        http_response_code(200);
 
-        return json_encode($resultQuery, JSON_PRETTY_PRINT);
+        header('Content-Type: application/json');
+        echo json_encode($resultQuery, JSON_PRETTY_PRINT);
+    }
+
+    public function create()
+    {
+        header('Content-Type: application/json');
+
+        $json = file_get_contents('php://input');
+
+        $data = json_decode($json, true);
+
+        if (isset($data['title']) && isset($data['description'])) {
+            $title = $data['title'];
+            $description = $data['description'];
+
+            $sqlInsert = $this->connection->prepare("INSERT INTO $this->table (`title`, `description`) VALUES (?, ?)");
+            $sqlInsert->execute([$title, $description]);
+
+            $insertedId = $this->connection->lastInsertId();
+
+            if ($insertedId) {
+                $response = [
+                    'message' => 'Task created successfully',
+                    'inserted_id' => $insertedId
+                ];
+                http_response_code(200);
+                echo json_encode($response, JSON_PRETTY_PRINT);
+            } else {
+                $response = [
+                    'message' => 'Failed to create task'
+                ];
+                http_response_code(500);
+                echo json_encode($response, JSON_PRETTY_PRINT);
+            }
+
+        } else {
+            $response = [
+                'message' => 'Title and description are required fields'
+            ];
+
+            http_response_code(400); // Bad Request
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        }
     }
 }

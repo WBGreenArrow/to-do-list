eed0c13b5a084a23434d1564f0ba860493d2ff5a
Wellyson E Brito <wellysone.brito@gmail.com>
2023-06-22 16:27:28 -0300

    feat: added createTask method in the routes

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,7 @@
 
 $routes = [
     ['method' => 'GET', 'route' => 'tasks', 'handler' => 'TasksController@getAllTasks'],
-    ['method' => 'POST', 'route' => 'tasks', 'handler' => 'TasksController@create'],
+    ['method' => 'POST', 'route' => 'tasks', 'handler' => 'TasksController@createTask'],
     ['method' => 'DELETE', 'route' => 'tasks/{id}', 'handler' => 'TaskController@delete'],
 ];
 
